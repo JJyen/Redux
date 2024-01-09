22cc86c01c75e2a4defd99d6414c301285d5bb19
JJyen <gdb1632@naver.com>
2024-01-09 15:07:19 +0900

    reduxtoolkit-createAction

--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,44 +1,40 @@
-import {legacy_createStore as createStore} from "redux"
+import {legacy_createStore as createStore} from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE"
 
-//action creator
-const addTodo = (text) => {
-    return {
-        type: ADD,
-        text,
-    }
-};
+// const ADD = "ADD";
+// const DELETE = "DELETE"
 
-const deleteTodo = (id) => {
-    return {
-        type: DELETE,
-        id: parseInt(id),
-    }
-}
-
-// const reducer = (state=[],action) => {
-//     switch(action.type){
-//         case ADD:
-//             return [{text: action.text, id: Date.now()}, ...state];
-//         case DELETE:
-//             return state.filter(todo => todo.id !== action.id);
-//         default:
-//             return state;
-//     };
+// //action creator
+// const addTodo = (text) => {
+//     return {
+//         type: ADD,
+//         text,
+//     }
 // };
+
+// const deleteTodo = (id) => {
+//     return {
+//         type: DELETE,
+//         id: parseInt(id),
+//     }
+// }
+
+const addTodo = createAction("ADD");
+const deleteTodo = createAction("DELETE");
 
 const storedTodo = JSON.parse(localStorage.getItem("todo"));
 
 const reducer = (state = storedTodo || null, action) => {
+  console.log(action, addTodo(), deleteTodo());
+
   switch(action.type){
-    case ADD:
-      const newTodos = [{text: action.text, id: Date.now()}, ...(state)];
+    case addTodo.type:
+      const newTodos = [{text: action.payload, id: Date.now()}, ...state];
       localStorage.setItem("todo", JSON.stringify(newTodos));
       return newTodos;
-    case DELETE:
-      const delTodo = (state).filter(todo => todo.id !== action.id);
+    case deleteTodo.type:
+      const delTodo = state.filter(todo => todo.id !== action.payload);
       localStorage.setItem("todo", JSON.stringify(delTodo));
       return delTodo;
     default:
