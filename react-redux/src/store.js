f3879615c44f164399b4a52881307794420a606c
JJyen <gdb1632@naver.com>
2024-01-09 23:40:35 +0900

    reduxtoolkit-createReducer

--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,46 +1,60 @@
 import {legacy_createStore as createStore} from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 
-// const ADD = "ADD";
-// const DELETE = "DELETE"
+/*const ADD = "ADD";
+const DELETE = "DELETE"
 
-// //action creator
-// const addTodo = (text) => {
-//     return {
-//         type: ADD,
-//         text,
-//     }
-// };
+//action creator
+const addTodo = (text) => {
+    return {
+        type: ADD,
+        text,
+    }
+};
 
-// const deleteTodo = (id) => {
-//     return {
-//         type: DELETE,
-//         id: parseInt(id),
-//     }
-// }
+const deleteTodo = (id) => {
+    return {
+        type: DELETE,
+        id: parseInt(id),
+    }
+}
 
-const addTodo = createAction("ADD");
-const deleteTodo = createAction("DELETE");
-
-const storedTodo = JSON.parse(localStorage.getItem("todo"));
-
-const reducer = (state = storedTodo || null, action) => {
-  console.log(action, addTodo(), deleteTodo());
-
+const reducer = (state = storedTodo, action) => {
   switch(action.type){
-    case addTodo.type:
-      const newTodos = [{text: action.payload, id: Date.now()}, ...state];
+    case ADD:
+      const newTodos = [{text: action.text, id: Date.now()}, ...(state)];
       localStorage.setItem("todo", JSON.stringify(newTodos));
       return newTodos;
-    case deleteTodo.type:
-      const delTodo = state.filter(todo => todo.id !== action.payload);
+    case DELETE:
+      const delTodo = (state).filter(todo => todo.id !== action.id);
       localStorage.setItem("todo", JSON.stringify(delTodo));
       return delTodo;
     default:
       return state;
   }
-}
+}*/
+
+const addTodo = createAction("ADD");
+const deleteTodo = createAction("DELETE");
+
+const getStoredTodo = localStorage.getItem("todo");
+const storedTodo = getStoredTodo ? JSON.parse(getStoredTodo) : [];
+
+const reducer = createReducer(storedTodo, builder => {
+  builder
+    .addCase(addTodo, (state, action) => {
+      const newTodos = state.concat({text: action.payload, id: Date.now()}); //....
+      localStorage.setItem("todo",JSON.stringify(newTodos))
+      return newTodos;
+    })
+    .addCase(deleteTodo, (state, action) => {
+      const delTodo = state.filter(todo => todo.id !== action.payload);
+      localStorage.setItem("todo", JSON.stringify(delTodo))
+      return delTodo;
+    })
+})
+
 
 const store = createStore(reducer);
 
